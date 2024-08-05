a1c373f8ac5dfe6d95a3efba5ea42b9ca58a24ba
Uriel Fruelda <urielfruelda@gmail.com>
2024-08-05 23:56:32 +0800

    update

---
+++ sunhilllms/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',  // Update this based on your project structure
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class',  // Enable class-based dark mode
+  plugins: [],
+};
